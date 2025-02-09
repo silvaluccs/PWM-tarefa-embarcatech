6963f8e9044e89406686edc176c5960a48052b15
Lucas Silva <kucasss19@gmail.com>
2025-02-09 12:01:15 -0300

    ajuste rotações e refatoração

--- main.c
+++ main.c
@@ -5,12 +5,19 @@
 const uint pino_servo = 22;
 const uint pino_led = 11;
 
-void setup_led();
-void setup_pwm(uint pino, uint *slice);
-void girar_servo_180(uint pino, uint slice);
-void girar_servo_90(uint pino, uint slice);
-void girar_servo_0(uint pino, uint slice);
-void mover_servo_suavemente(uint pino, uint slice, uint inicio, uint fim);
+const uint zero_graus = 1638;  // 2,5% de 65535 -> 1638
+const uint noventa_graus = 4817;  // 7,35% de 65535 -> 4817
+const uint cento_oitenta_graus = 7864;  // 12% de 65535 -> 7864
+
+const float frequencia_pwm = 38.1f;  // Frequência de 50 Hz
+const uint wrap_pwm = 65535;  // 65535 passos
+
+void setup_led(); // Protótipo da função para configurar o pino do LED
+void setup_pwm(uint pino, uint *slice); // Protótipo da função para configurar o pino do servo com frequência de 50 Hz
+void girar_servo_180(uint pino, uint slice); // Protótipo da função para girar o servo para 180 graus (Duty Cycle de 12%)
+void girar_servo_90(uint pino, uint slice); // Protótipo da função para girar o servo para 90 graus (Duty Cycle de 7,35%)
+void girar_servo_0(uint pino, uint slice); // Protótipo da função para girar o servo para 0 graus (Duty Cycle de 2,5%)
+void mover_servo_suavemente(uint pino, uint slice, uint inicio, uint fim); // Protótipo da função para mover o servo suavemente de um ângulo para outro
 
 int main() {
     stdio_init_all();
@@ -22,9 +29,14 @@ int main() {
 
     while (true) {
 
-        girar_servo_180(pino_servo, slice);
-    
+        girar_servo_180(pino_servo, slice); // 0° -> 180°
+        girar_servo_90(pino_servo, slice); // 180° -> 90°
+        girar_servo_0(pino_servo, slice); // 90° -> 0° 
+        
+        mover_servo_suavemente(pino_servo, slice, zero_graus, cento_oitenta_graus);  // 0° -> 180°
+        mover_servo_suavemente(pino_servo, slice, cento_oitenta_graus, zero_graus);  // 180° -> 0°
 
+        sleep_ms(1000);  // Atraso de 1 segundo
     }
 }
 
@@ -37,43 +49,54 @@ void setup_led() {
     gpio_put(pino_led, 0);
 }
 
+
 /*
  * Função para configurar o pino do servo com frequência de 50 Hz
  */
 void setup_pwm(uint pino, uint *slice) {
     gpio_set_function(pino, GPIO_FUNC_PWM);  // Habilitar o pino GPIO como PWM
     *slice = pwm_gpio_to_slice_num(pino);    // Obter o canal PWM da GPIO
-    pwm_set_clkdiv(*slice, 38.1f);           // Define o divisor de clock do PWM
-    pwm_set_wrap(*slice, 65535);              // Define o valor de wrap (4000 passos)
+    pwm_set_clkdiv(*slice, frequencia_pwm);           // Define o divisor de clock do PWM
+    pwm_set_wrap(*slice, wrap_pwm);              // Define o valor de wrap (4000 passos)
 }
+
 
 /*
  * Função para girar o servo para 180 graus (Duty Cycle de 12%)
  */
 void girar_servo_180(uint pino, uint slice) {
-    pwm_set_gpio_level(pino, 7864);  
+    pwm_set_gpio_level(pino, cento_oitenta_graus);  // 12% de 65535 -> 7864
     pwm_set_enabled(slice, true);   // Habilita o PWM no slice correspondente
     sleep_ms(5000);                 // Aguarda 5 segundos
 }
 
+
+/*
+ * Função para girar o servo para 90 graus (Duty Cycle de 7,35%)
+ */
 void girar_servo_90(uint pino, uint slice) {
-    pwm_set_gpio_level(pino, 294);  // 7,35% de 4000 -> 294
+    pwm_set_gpio_level(pino, noventa_graus); // 7,35% de 65535 -> 4817  
     pwm_set_enabled(slice, true);   // Habilita o PWM no slice correspondente
     sleep_ms(5000);                 // Aguarda 5 segundos
 }
+
 
 /*
  * Função para girar o servo para 0 graus (Duty Cycle de 2,5%)
  */
 void girar_servo_0(uint pino, uint slice) {
-    pwm_set_gpio_level(pino, 100);  // 2,5% de 4000 -> 100
+    pwm_set_gpio_level(pino, zero_graus);  // 2,5% de 65535 -> 1638
     pwm_set_enabled(slice, true);   // Habilita o PWM no slice correspondente
     sleep_ms(5000);                 // Aguarda 5 segundos
 }
 
+
+/*
+ * Função para mover o servo suavemente de um ângulo para outro
+ */
 void mover_servo_suavemente(uint pino, uint slice, uint inicio, uint fim) {
-    int passo = (inicio < fim) ? 1 : -1;
-    for (uint nivel = inicio; nivel != fim; nivel += passo) {
+    int passo = (inicio < fim) ? 16 : -16;
+    for (int nivel = inicio; nivel != fim; nivel += passo) {
         pwm_set_gpio_level(pino, nivel);
         sleep_ms(10);  // Atraso de 10 ms para suavizar o movimento
     }
